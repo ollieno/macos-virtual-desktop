03f390508d0cdc310bc5f25355840befe7f5ed70
Jeroen Olthof <jeroen@wedevelop.nl>
2026-03-18 20:40:50 +0100

    feat: add launch-at-login via SMAppService

---
+++ VirtualDesktop/Services/LaunchAtLogin.swift
@@ -0,0 +1,23 @@
+import ServiceManagement
+
+enum LaunchAtLogin {
+    static var isEnabled: Bool {
+        SMAppService.mainApp.status == .enabled
+    }
+
+    static func enable() {
+        try? SMAppService.mainApp.register()
+    }
+
+    static func disable() {
+        try? SMAppService.mainApp.unregister()
+    }
+
+    static func toggle() {
+        if isEnabled {
+            disable()
+        } else {
+            enable()
+        }
+    }
+}
